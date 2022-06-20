507a4fcb7701fcc0c9cbb7ddaf88af91e496b1a4
Jock Dalby <jock.dalby@citrusad.com>
2022-06-20 17:55:43 +1000

    write message to the new file via the server

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
+const fs = require('fs');
 const http = require('http');
 
 // This function will be executed whenever a request reaches the server.
 function requestListener(req, res) {
-  const url = req.url;
+  const { url, method } = req;
   if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>Enter Message</title></head>');
     res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>');
     res.write('</html>');
     // return not needed to return response, but to exit the function
     return res.end();  
+  }
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    // Every time a chunk of incoming data from a request has been parsed, the below
+    // method will be executed. Here we are collecting all data from the request
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    });
+    // Executed when finished parsing the request
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString(); // message=<message>
+      const message = parsedBody.split('=')[1]; // actual message
+      fs.writeFileSync('message.txt', message); // write message to file
+    });
+    // write multiple Header metadata in one go
+    // Can also be written as res.statusCode = 302 and then use res.setHeader for each header.
+    // Location redirects req back to '/' route (above)
+    res.writeHead(302, { 'Location': '/' })
+    return res.end();
   }
   // type of content which is part of response is HTML
   res.setHeader('Content-Type', 'text/html');
