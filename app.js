2432568e03a8875e9e91fab7dfb6650274f03ff7
Jock Dalby <jock.dalby@citrusad.com>
2022-06-20 15:19:55 +1000

    call process.exit to shutdown server

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const http = require('http');
 // This function will be executed whenever a request reaches the server.
 function requestListener(req, res) {
   console.log(req)
+  setTimeout(() => {
+    process.exit()
+  }, 3000)
 }
 
 const server = http.createServer(requestListener);
