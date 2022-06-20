e420cd5b09b1cd307adcd5d81bc0d68e14c9c4fa
Jock Dalby <jock.dalby@citrusad.com>
2022-06-20 15:37:49 +1000

    send a basic html response from our client back to the server

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const http = require('http');
 
 // This function will be executed whenever a request reaches the server.
 function requestListener(req, res) {
-  console.log(req)
-  setTimeout(() => {
-    process.exit()
-  }, 3000)
+  console.log(req.url, req.method, req.headers)
+  // type of content which is part of response is HTML
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hello from my Node.js server!</h1></body>');
+  res.write('</html>');
+  // Nothing else after end, because end is when server will send response to client.
+  res.end();
 }
 
 const server = http.createServer(requestListener);
