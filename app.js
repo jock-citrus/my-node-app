d5cccb7d8ddaa3ea5bbc624c6fc0576c8f664bfd
Jock Dalby <jock.dalby@citrusad.com>
2022-06-20 17:30:46 +1000

    setup route to return basic form when user sends request to server for '/'

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const http = require('http');
 
 // This function will be executed whenever a request reaches the server.
 function requestListener(req, res) {
-  console.log(req.url, req.method, req.headers)
+  const url = req.url;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    // return not needed to return response, but to exit the function
+    return res.end();  
+  }
   // type of content which is part of response is HTML
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
