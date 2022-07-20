916c26da46707f8778b3f0ea0fe24f630cc7c070
Jock Dalby <jock.dalby@citrusad.com>
2022-07-21 08:41:41 +1000

    setup middleware for parsing incoming requests

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+// Registers a middleware which parses the req.body before calling next()
+// and passing on to next middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', (req, res, next) => {
   console.log('This is middleware that always runs before calling "next" and passing on to next middleware')
   next();
-})
+});
 
 app.use('/add-product', (req, res, next) => {
-  res.send('<p>The "Add Product" Page</p>'); // express will interpret and add header for text/html
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'); // express will interpret and add header for text/html
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 // '/' must go last because every route will be matched with 'starts with' '/'
