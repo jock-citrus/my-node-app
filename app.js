06bf5d628bc104ea3f1866b8fb303dcf99b18f9f
Jock Dalby <jock.dalby@citrusad.com>
2022-07-20 14:44:42 +1000

    add middleware to express instance

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const express = require('express');
 
 const app = express();
 
+// next allows the request to continue to the next middleware in line
+app.use((req, res, next) => {
+  console.log("In middleware 1");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("In middleware 2");
+});
+
 const server = http.createServer(app);
 
 // calling listen will keep node running
