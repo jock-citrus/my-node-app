c23e7c33d7cfc418e1c5b57b095cc08407030c84
Jock Dalby <jock.dalby@citrusad.com>
2022-07-27 09:12:22 +1000

    install and implement pug templating

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 
-const rootDir = require('./util/path')
+const rootDir = require('./util/path');
 
 const app = express();
 
-const adminData = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+// Set global config value on express app
+app.set('view engine', 'pug');
+// Telling express app that the views are in the /views dir.
+// This is the default location express would look for,
+// but leaving here as an example.
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 // Registers a middleware which parses the req.body before calling next()
 // and passing on to next middleware
@@ -15,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // pass in a folder we want to grant read access to.
 // we want to grant access to the public dir so in the HTML
 // files we can reference the stylesheets
-app.use(express.static(path.join(rootDir, 'public')))
+app.use(express.static(path.join(rootDir, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
@@ -23,7 +30,7 @@ app.use(shopRoutes);
 // catch all filter for any requests that don't match any routes
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
-})
+});
 
 // calling listen will keep the server running
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -11,9 +11,9 @@ const router = express.Router();
 // if using .use, '/' must go last because every route will be matched with 'starts with' '/'
 // when using .get, it needs to be an exact match, so can go in any order
 router.get('/', (req, res, next) => {
-  // path.join will build the path in a way which works on both Linux and Windows systems.
-  console.log(adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  // render will use templating engine defined in app.js, and will look in views dir for templating
+  // file of same name. This will render shop.pug.
+  res.render('shop')
 });
 
 module.exports = router;
