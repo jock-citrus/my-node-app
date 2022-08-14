5d266516da2088ca03be8301758fbe92afd1b812
Jock Dalby <jock.dalby@citrusad.com>
2022-08-15 09:04:15 +1000

    add Product model

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,12 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('add-product', { docTitle: 'Add Product', path: '/admin/add-product' });
 }
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect('/');
 }
 
@@ -13,6 +14,6 @@ exports.getProducts = (req, res, next) => {
   res.render('shop', {
     docTitle: 'Shop',
     path: '/admin/shop',
-    prods: products
+    prods: Product.fetchAll()
   })
 }
