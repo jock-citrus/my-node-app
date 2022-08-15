69fb8dc98f272dd2844b5ecd8e6aed98dac9d6a9
Jock Dalby <jock.dalby@citrusad.com>
2022-08-16 09:30:34 +1000

    add Orders route and view

--- controllers/shop.js
+++ controllers/shop.js
@@ -24,6 +24,10 @@ exports.getCart = (req, res, next) => {
   res.render('shop/cart', { docTitle: 'Cart', path: '/cart' });
 }
 
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', { docTitle: 'Orders', path: '/orders' });
+}
+
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', { docTitle: 'Checkout', path: '/checkout' });
 }
