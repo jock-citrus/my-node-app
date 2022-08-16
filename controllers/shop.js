ec7cdb4d99112ecf632427cff7de24149fb9886c
Jock Dalby <jock.dalby@citrusad.com>
2022-08-17 09:13:05 +1000

    render product detail view

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,6 +10,18 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const { productId } = req.params;
+
+  Product.findById(productId, product => {
+    res.render('shop/product-detail', {
+      product,
+      pageTitle: product.title,
+      path: '/products'
+    });
+  });
+};
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/index', {
