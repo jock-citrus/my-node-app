5a96867319a5ff2c2efebdd0f9c3b0286b7f4a5c
Jock Dalby <jock.dalby@citrusad.com>
2022-08-18 09:03:36 +1000

    update the edit-product template to prepopulare with existing data

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,13 +1,32 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    editing: false
   });
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editing = Boolean(req.query.edit);
+  if (!editing) {
+    return res.redirect('/');
+  }
+
+  const {productId} = req.params;
+  
+  Product.findById(productId, product => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing,
+      product
+    });
+  })
 };
 
 exports.postAddProduct = (req, res, next) => {
