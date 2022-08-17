38eac1ad098044b8c008e75f24d44befcc59417b
Jock Dalby <jock.dalby@citrusad.com>
2022-08-18 09:12:17 +1000

    wire up edit-product route

--- controllers/admin.js
+++ controllers/admin.js
@@ -39,6 +39,16 @@ exports.postAddProduct = (req, res, next) => {
   res.redirect('/');
 };
 
+exports.postEditProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(title, imageUrl, description, price);
+  product.save();
+  res.redirect('/');
+};
+
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('admin/products', {
