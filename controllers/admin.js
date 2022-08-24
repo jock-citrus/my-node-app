f938ab2789041ada6abd55408badf3f672c1e421
Jock Dalby <jock.dalby@citrusad.com>
2022-08-25 09:17:27 +1000

    update admin.js controller functions to use sequelize methods

--- controllers/admin.js
+++ controllers/admin.js
@@ -16,13 +16,16 @@ exports.getEditProduct = (req, res, next) => {
 
   const {productId} = req.params;
   
-  Product.findById(productId)
-    .then(([rowData]) => {
+  Product.findByPk(productId)
+    .then(product => {
+      if (!product) {
+        return res.redirect('/');
+      }
       res.render('admin/edit-product', {
         pageTitle: 'Edit Product',
         path: '/admin/edit-product',
         editing,
-        product: rowData[0]
+        product
       });
     }).catch(err => console.log('Product.findById', err))
 };
@@ -38,10 +41,16 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.postEditProduct = (req, res, next) => {
-  const {productId, title, imageUrl, price, description} = req.body;
-  const product = new Product(productId, title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  const {productId, title, price, imageUrl, description} = req.body;
+  Product.findByPk(productId).then(product => {
+    product.title = title;
+    product.price = price;
+    product.imageUrl = imageUrl;
+    product.description = description;
+    return product.save();
+  })
+  .then(() => res.redirect('/'))
+  .catch(err => console.log('exports.postEditProduct', err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
@@ -51,9 +60,9 @@ exports.postDeleteProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll().then(([rows]) => {
+  Product.findAll().then(products => {
     res.render('admin/products', {
-      prods: rows,
+      prods: products,
       pageTitle: 'Admin Products',
       path: '/admin/products'
     });
